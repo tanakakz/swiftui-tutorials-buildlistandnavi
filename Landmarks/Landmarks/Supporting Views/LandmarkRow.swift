669e099685459e9340bf3e6602ad10a29897596d
tanakakz <tanakakz.feb.23@gmail.com>
2020-01-07 21:08:23 +0900

    add Landmark Row View.

---
+++ Landmarks/Landmarks/Supporting Views/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by tkz on 2020/01/07.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
