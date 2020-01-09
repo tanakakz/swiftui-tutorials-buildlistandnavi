71109aaf5e108678e6e4e183bfb85b2ad853d43a
tanakakz <tanakakz.feb.23@gmail.com>
2020-01-09 15:23:30 +0900

    impl list mock.

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by tkz on 2020/01/08.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
